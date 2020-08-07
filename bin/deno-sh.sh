877fd983c9660a3e699742d6be9fa90415bf8426
Bert van Brakel <bertvanbrakel@gmail.com>
2020-08-07 12:55:51 +1000

    remove the use of DENO_SCRIPT env var as no longer needed

--- bin/deno-sh.sh
+++ bin/deno-sh.sh
@@ -4,6 +4,4 @@ file=$(readlink -f $1)
 deno_args="$(head -3 "$file" | grep 'deno-args:' | cut -d ':' -f2)"
 #echo "deno-sh: running: 'deno run $deno_args $@'"
 #echo "deno-sh: file: $file"
-
-export DENO_ENTRY_SCRIPT="$file"
-deno run $deno_args --allow-env "$@"
+deno run $deno_args "$@"
